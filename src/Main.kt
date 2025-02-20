3f78bd98aec923b574d13670eb0ab07a2b2a1abd
Sabelo Thabede <sabelothabede01@gmail.com>
2025-02-20 12:21:13 +0200

    Commit the changes

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "Kotlin"
+    val surname = "Why" 
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
